af50ba49e533de495689af083eb94219f8399d80
Raphael <raphrien@gmail.com>
2021-04-15 08:53:38 -0400

    created project

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Informatique 21 on 2021-04-15.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
